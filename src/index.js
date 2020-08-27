85626e5c4c6338975473c5fd040910753ede64e9
TANGSHIZHE <2417024719@qq.com>
2020-08-27 17:43:11 +0800

    回退版本，管理输出，使用hash

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml'
+// import './style.css';
+// import Icon from './icon.png'
+// import Data from './data.xml'
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash，现在由这个脚本导入
   element.innerHTML = _.join(['Hello', 'webpack12'], ' ');
-  element.classList.add('hello');
+//   element.classList.add('hello');
 
   // 将图像添加到我们现有的 div。
-   var myIcon = new Image();
-   myIcon.src = Icon;
+//    var myIcon = new Image();
+//    myIcon.src = Icon;
 
-   element.appendChild(myIcon);
+//    element.appendChild(myIcon);
 
-   console.log(Data);
-
+//    console.log(Data);
+ 
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,58 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
   module.exports = {
     entry: [
         './src/index.js',
     ],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Caching'
+        })
+    ],
     output: {
-      filename: 'bundle.js',
+        filename: '[name].[chunkhash].js',
       path: path.resolve(__dirname, 'dist')
     },
-    module: {
-      rules: [
-        //   加载css
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        // 加载图片资源
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-        },
-        // 加载字体
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-                'file-loader'
-            ]
-        },
-        // 加载数据 如 JSON 文件，CSV、TSV 和 XML
-        {
-            test: /\.(csv|tsv)$/,
-            use: [
-                'csv-loader'
-            ]
-        },
-        {
-            test: /\.xml$/,
-            use: [
-                'xml-loader'
-            ]
-        }
-      ]
-    }
+    // module: {
+    //   rules: [
+    //     //   加载css
+    //     {
+    //       test: /\.css$/,
+    //       use: [
+    //         'style-loader',
+    //         'css-loader'
+    //       ]
+    //     },
+    //     // 加载图片资源
+    //     {
+    //         test: /\.(png|svg|jpg|gif)$/,
+    //         use: [
+    //             'file-loader'
+    //         ]
+    //     },
+    //     // 加载字体
+    //     {
+    //         test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //         use: [
+    //             'file-loader'
+    //         ]
+    //     },
+    //     // 加载数据 如 JSON 文件，CSV、TSV 和 XML
+    //     {
+    //         test: /\.(csv|tsv)$/,
+    //         use: [
+    //             'csv-loader'
+    //         ]
+    //     },
+    //     {
+    //         test: /\.xml$/,
+    //         use: [
+    //             'xml-loader'
+    //         ]
+    //     }
+    //   ]
+    // }
   };
