123d9fb1a0d8d9ae5e210c851af5f4d7914a791a
TANGSHIZHE <2417024719@qq.com>
2020-08-28 11:29:39 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml'
+// import './style.css';
+// import Icon from './icon.png'
+// import Data from './data.xml'
+import printMe from './print';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button')
 
   // Lodash，现在由这个脚本导入
   element.innerHTML = _.join(['Hello', 'webpack12'], ' ');
-  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe
+  element.appendChild(btn)
+  // element.classList.add('hello');
 
   // 将图像添加到我们现有的 div。
-   var myIcon = new Image();
-   myIcon.src = Icon;
+  //  var myIcon = new Image();
+  //  myIcon.src = Icon;
 
-   element.appendChild(myIcon);
+  //  element.appendChild(myIcon);
 
-   console.log(Data);
+  //  console.log(Data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
   module.exports = {
-    entry: [
-        './src/index.js',
+    entry: {
+      app: './src/index.js',
+      print: './src/print.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Output Mangement'
+      }),
+      new CleanWebpackPlugin()
     ],
     output: {
-      filename: 'bundle.js',
+      filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist')
     },
-    module: {
-      rules: [
-        //   加载css
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        // 加载图片资源
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-        },
-        // 加载字体
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-                'file-loader'
-            ]
-        },
-        // 加载数据 如 JSON 文件，CSV、TSV 和 XML
-        {
-            test: /\.(csv|tsv)$/,
-            use: [
-                'csv-loader'
-            ]
-        },
-        {
-            test: /\.xml$/,
-            use: [
-                'xml-loader'
-            ]
-        }
-      ]
-    }
   };
